28da836900a9c18a12acba454b60f350d36abe7d
meturgut <muhammedturgutdev@gmail.com>
2021-07-18 14:29:18 +0300

    code refactoring

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+
+public class Hero : Character
+{
+    public override void Move()
+    {
+        base.Move();
+        currentCell = GameManager.Instance.WorldPointToCell(transform.position);
+
+        if (currentNodeIndex == 0)
+        {
+            Debug.Log("Win");
+            GameManager.Instance.isLevelFinished = true;
+        }
+    }
+}
