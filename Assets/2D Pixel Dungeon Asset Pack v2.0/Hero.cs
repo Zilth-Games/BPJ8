7397f57ed6dca08bb5bb7287ff1379c84565e936
meturgut <muhammedturgutdev@gmail.com>
2021-07-18 10:46:15 +0300

    pathfinding system was added

---
+++ Assets/2D Pixel Dungeon Asset Pack v2.0/Hero.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    public Vector3Int[,] nodes;
+    Astar astar;
+    List<Node> roadPath = new List<Node>();
+
+    public Vector2Int start;
+    public Vector2Int target;
+
+
+    void Start()
+    {
+        nodes = GameManager.Instance.CreateGrid();
+        astar = new Astar(nodes, GameManager.Instance.Bounds.size.x, GameManager.Instance.Bounds.size.x);
+        FindPath();
+    }
+
+    public void FindPath()
+    {
+        nodes = GameManager.Instance.CreateGrid();
+
+        if (roadPath != null && roadPath.Count > 0)
+            roadPath.Clear();
+
+        roadPath = astar.CreatePath(nodes, start, target, 1000);
+        if (roadPath == null)
+            return;
+
+        DrawRoad();
+        //start = new Vector2Int(roadPath[0].X, roadPath[0].Y);
+    }
+    private void DrawRoad()
+    {
+        for (int i = 0; i < roadPath.Count; i++)
+        {
+            GameManager.Instance.SetTileToRoadMap(roadPath[i].X, roadPath[i].Y);
+        }
+    }
+}
---
+++ Assets/Scripts/2D Pathfinding/Node.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+public class Node
+{
+    public int X;
+    public int Y;
+    public int F;
+    public int G;
+    public int H;
+    public int Height = 0;
+    public List<Node> Neighboors;
+    public Node previous = null;
+    public Node(int x, int y, int height)
+    {
+        X = x;
+        Y = y;
+        F = 0;
+        G = 0;
+        H = 0;
+        Neighboors = new List<Node>();
+        Height = height;
+    }
+    public void AddNeighboors(Node[,] grid, int x, int y)
+    {
+        if (x < grid.GetUpperBound(0))
+            Neighboors.Add(grid[x + 1, y]);
+        if (x > 0)
+            Neighboors.Add(grid[x - 1, y]);
+        if (y < grid.GetUpperBound(1))
+            Neighboors.Add(grid[x, y + 1]);
+        if (y > 0)
+            Neighboors.Add(grid[x, y - 1]);
+        #region diagonal
+        //if (X > 0 && Y > 0)
+        //    Neighboors.Add(grid[X - 1, Y - 1]);
+        //if (X < Utils.Columns - 1 && Y > 0)
+        //    Neighboors.Add(grid[X + 1, Y - 1]);
+        //if (X > 0 && Y < Utils.Rows - 1)
+        //    Neighboors.Add(grid[X - 1, Y + 1]);
+        //if (X < Utils.Columns - 1 && Y < Utils.Rows - 1)
+        //    Neighboors.Add(grid[X + 1, Y + 1]);
+        #endregion
+    }
+
+
+}
